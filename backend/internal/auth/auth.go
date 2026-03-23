b14025965c2d7aa93cbcd421316d26f023482682
NKS01X <nikhilayush2020@gmail.com>
2026-03-24 00:51:12 +0530

    done with auth

--- backend/internal/auth/auth.go
+++ backend/internal/auth/auth.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginRequest struct {
@@ -24,6 +25,39 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
+type SignupResponse struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+func Signup(c *gin.Context) {
+	var req SignupResponse
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if req.Username == "" || req.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username and password required"})
+		return
+	}
+	// encrpting the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "security error"})
+		return
+	}
+	//save the paylod in the db
+	//
+	var payload SignupResponse
+	payload.Email = req.Email
+	payload.Password = string(hashedPassword)
+	payload.Email = req.Email
+	// save it in the database
+	c.JSON(http.StatusOK, gin.H{"message": "User created succesfully"})
+}
+
 func Login(c *gin.Context) {
 	var req LoginRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
@@ -35,7 +69,13 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "username and password required"})
 		return
 	}
-
+	// db-pass
+	fetchedpass := ""
+	err := bcrypt.CompareHashAndPassword([]byte(fetchedpass), []byte(req.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		return
+	}
 	secret := os.Getenv("JWT_SECRET")
 	if secret == "" {
 		secret = "default-secret-change-in-production"
@@ -76,7 +116,6 @@ func ValidateToken(tokenString string) (*Claims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(secret), nil
 	})
-
 	if err != nil {
 		return nil, err
 	}
